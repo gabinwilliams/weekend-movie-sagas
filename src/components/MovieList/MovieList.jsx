9a82a3ab667eab7d8d7c2eb2d74f51546409bb16
gabinwilliams <scoutsnknives@gmail.com>
2021-04-11 17:31:58 -0500

    updated styles on MovieList page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import { useDispatch, useSelector } from 'react-redux';
 import {useHistory} from 'react-router-dom';
-import './MovieList.css'
+import './MovieList.css';
+
+
+const useStyles = makeStyles({
+    root: {
+      display: 'flex',
+      justifyContent: 'center',
+      maxWidth: 345,
+      padding: 10,
+      margin: 5,
+    
+      
+    },
+    image: {
+        display: 'flex',
+        justifyContent: 'center',
+        objectFit: 'contain',
+        height: 300,
+        width: 300,
+        
+    },
+    title: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+    }
+  });
+
+
 
 function MovieList() {
     const history = useHistory();
@@ -10,6 +46,10 @@ function MovieList() {
     const movies = useSelector(store => store.movies);
     const genres = useSelector(store => store.genres);
     const clickedMovieGenre = useSelector(store => store.clickedMovieGenre);
+
+    const classes = useStyles();
+
+   
     
     
     
@@ -26,8 +66,8 @@ function MovieList() {
     const clickedMovie = (event) => {
         event.preventDefault();
         console.log('clicked:', event.target.id);
-
-        dispatch({type: 'RESET', payload: clickedMovieGenre})
+// resets the clickedMovieGenre so it doesn't double render genres
+        dispatch({type: 'RESET', payload: clickedMovieGenre});
 
         movies.map(movie => {
             if(movie.id == event.target.id) {
@@ -54,15 +94,46 @@ function MovieList() {
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div 
-                            key={movie.id} >
-                            <h3>{movie.title}</h3>
 
-                                <img src={movie.poster} alt={movie.title} id={movie.id} 
-                                    onClick={clickedMovie} />
+
+                        <Card key={movie.id} className={classes.root}>
+                        <CardActionArea>
+                          <CardMedia
+                            className={classes.image}
+                            onClick={clickedMovie}
+                            id={movie.id}
+                            component="img"
+                            alt={movie.title}
+                            image={movie.poster}
+                            title="Poster"
+                          />
+                          <CardContent>
+                            <Typography className={classes.title} gutterBottom variant="h5" component="h2">
+                                {movie.title}
+                            </Typography>
+                            
+                          </CardContent>
+                        </CardActionArea>
+                        {/* <CardActions>
+                          <Button size="small" color="primary">
+                            Share
+                          </Button>
+                          <Button size="small" color="primary">
+                            Learn More
+                          </Button>
+                        </CardActions> */}
+                      </Card>
+
+
+                        // <div 
+                        //     key={movie.id} >
+                        //     <h3>{movie.title}</h3>
+
+                        //         <img src={movie.poster} alt={movie.title} id={movie.id} 
+                        //             onClick={clickedMovie} />
                             
                            
-                        </div>
+                        
                     );
                 })}
             </section>
